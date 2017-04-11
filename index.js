17baa650c3a641bb233993c836c24b05b08976ab
Rhio Kim <rhio.kim@gmail.com>
2017-04-11 11:59:21 +0900

    refactor(app): remove www dir as document root

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const express = require('express')
 const path = require('path')
 const displayRoutes = require('express-routemap')
 
+const app = express()
 const PORT = process.env.PORT || 8082
 
 const routeSample = require('./routes/sample.route')
 
-const app = express()
-app.use(express.static(path.join(__dirname, '..', 'www')))
-
 app.use('/sample', routeSample)
+
+app.get('/', (req, res) => {
+  res.send('Hi World, I\'m seed-api-server')
+})
 
 http.createServer(app).listen(PORT, () => {
   displayRoutes(app)
